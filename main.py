7835832847d230083433e09651168efd1a0f75b0
kashish94 <kabhatt826@gmail.com>
2023-06-22 14:36:42 -0400

    changed the format of the file to get table, unfortunately the table ID on the inspect is genes-table, but when I run it it shows as None, meaning there is no table with that ID.

--- main.py
+++ main.py
@@ -6,15 +6,12 @@ soup = BeautifulSoup(source, "lxml")
 
 table = soup.find('table', id="genes-table")
 
-# Find all header rows (tr)
 header_rows = table.find_all("tr")
 for row in header_rows:
-    # Extract header cells (th)
     headers = row.find_all("th")
     header_texts = [header.text for header in headers]
     print(header_texts)
 
-# Find all data rows (tr)
 data_rows = table.find_all("tr")
 for row in data_rows:
     cells = row.find_all("td")
