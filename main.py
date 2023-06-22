d4d5026f4bc7adb190ef1633d5e2418494f44d69
kashish94 <kabhatt826@gmail.com>
2023-06-22 13:37:26 -0400

    deleted the code because the link did not work

--- main.py
+++ main.py
@@ -1,39 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 import csv
 
-
-class table_scraper:
-    results = []
-
-    def fetch(self, url):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-        }
-        response = requests.get(url, headers=headers)
-
-    def parse(self, html):
-        content = BeautifulSoup(html, 'lxml')
-        table = content.find_all('div', id='explore-case-table')
-        rows = table.find_all('th')
-        columns = table.find_all('tr')
-        self.results.append([header.text for header in rows])
-        print(self.results)
-        for row in columns:
-            self.results.append([data.text for data in row.find_all('td')])
-            for one in self.results:
-                print(one)
-
-    def to_csv(self):
-        with open('table_data.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(self.results)
-
-    def run(self):
-        response = self.fetch('https://portal.gdc.cancer.gov/exploration?searchTableTab=genes')
-        self.parse(response)
-
-
-if __name__ == '__main__':
-    scraper = table_scraper()
-    scraper.run()
