faed4dceb6fef61ce843a93966121cc1acc31e19
kashish94 <kabhatt826@gmail.com>
2023-06-29 16:51:18 -0400

    Changed the library to asyncio from beautiful soup. The extraction works and there is a CSV file.

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from bs4 import BeautifulSoup
-import requests
+import asyncio
+import csv
+import nest_asyncio
+from requests_html import AsyncHTMLSession
 
-source = requests.get('https://portal.gdc.cancer.gov/').text
-soup = BeautifulSoup(source, "lxml")
+nest_asyncio.apply()  # Enable nested asyncio support
 
-table = soup.find('table', id="genes-table")
+async def scrape_website():
+    session = AsyncHTMLSession()
+    response = await session.get("https://portal.gdc.cancer.gov/exploration?filters=%7B%22content%22%3A%5B%7B%22content%22%3A%7B%22field%22%3A%22cases.project.project_id%22%2C%22value%22%3A%5B%22TCGA-BRCA%22%5D%7D%2C%22op%22%3A%22in%22%7D%5D%2C%22op%22%3A%22and%22%7D")
+    await response.html.arender()
+    elements = response.html.find('.explore-case-table')
 
-header_rows = table.find_all("tr")
-for row in header_rows:
-    headers = row.find_all("th")
-    header_texts = [header.text for header in headers]
-    print(header_texts)
+    # Prepare data for CSV
+    data = []
+    for element in elements:
+        data.append(element.text)
 
-data_rows = table.find_all("tr")
-for row in data_rows:
-    cells = row.find_all("td")
-    cell_texts = [cell.text for cell in cells]
-    print(cell_texts)
+    # Write data to CSV
+    with open('data.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
 
+async def main():
+    await scrape_website()
+
+asyncio.run(main())
